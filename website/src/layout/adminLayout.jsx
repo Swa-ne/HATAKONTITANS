bf9318a3799c7322da98bb65b93a865e1dfca145
Swa-ne <spaulbautista@gmail.com>
2024-03-22 13:59:31 +0800

    Fixed bugs from login page and api

--- website/src/layout/adminLayout.jsx
+++ website/src/layout/adminLayout.jsx
@@ -1,8 +1,44 @@
 import { Outlet } from "react-router-dom";
+import SideBar from "../pages/admin/sidebar";
+import { useEffect, useState } from "react";
 export default function AdminLayout() {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+    const [isClicked, setIsClicked] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 768);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const handleMouseOver = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseOut = () => {
+        setIsHovered(false);
+    };
+    const handleOpenSideBar = () => {
+        setIsClicked(true);
+    };
+
+    const handleCloseSideBar = () => {
+        setIsClicked(false);
+    };
     return (
         <>
-            Admin Layout
+            <SideBar
+                isClicked={isClicked}
+                onClick={handleCloseSideBar}
+                onHover={handleMouseOver}
+                onUnhover={handleMouseOut}
+            />
             <Outlet />
         </>
     );
